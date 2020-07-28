ba48a38433c8b0a29d8d7c25d2784c5bf9a5fd41
Erkin <mackinkenny@gmail.com>
2020-07-28 15:37:36 +0600

    analityc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,6 +70,16 @@
     })
 </script>
 <script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://vk.com/js/api/openapi.js?168",t.onload=function(){VK.Retargeting.Init("VK-RTRG-511745-6Ozxq"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-511745-6Ozxq" style="position:fixed; left:-999px;" alt=""/></noscript>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-173937125-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-173937125-1');
+</script>
+
 </body>
 </html>
 
